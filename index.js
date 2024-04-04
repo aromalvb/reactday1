9e5be25fb1a4ee07c83c3aa3f995d42da0115465
aromalvb <165656826+aromalvb@users.noreply.github.com>
2024-04-04 15:43:24 +0530

    frontend

--- index.js
+++ index.js
@@ -1,59 +1,17 @@
-// 1.importing the express
-const express = require('express')
-const employeeModel = require("./model")
-const res = require('express/lib/response')
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-// 2.initalization
-const app = new express()
- //middleware ||  parsing the parameter
-app.use(express.urlencoded({extended:true}))
-app.use(express.json())
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
-// 3.api creation
-app.get('/',(req,res)=>{
-    res.send("This message is from the srever")
-})
-app.get('/Aromal',(req,res)=>{
-    res.send("Iam Aromal")
-})
-app.get('/Moto',(req,res)=>{
-    res.send("FREE FIRE")
-})
-app.get('/data',(req,res)=>{
-    res.json(
-        {
-        "name":"Aromal",
-        "age":19
-    }
-    )
-})
- 
-// api fot adding data
-app.post('/create',async(req,res)=>{
-var result = await new employeeModel(req.body)
-result.save()
-res.send("Data Added")
-})
-// api for viewing data
-app.get('/view',async(req,res)=>{
-    var data = await employeeModel.find()
-    res.json(data)
-    console.log(data)
-})
-// api for deleting data
-app.delete('/remove/:id',async(req,res)=>{
-    console.log(req.params)
-    let id = req.params.id
-    await employeeModel.findByIdAndDelete(id)
-    res.send("Deleted")
-})
-// api for updating data
-app.put('/edit/:id',async(req,res)=>{
-    let id = req.params.id
-    await employeeModel.findByIdAndUpdate(id,req.body)
-    res.send("Data Updataed")
-})
-// 4.port
-app.listen(3005,()=>{
-    console.log("port 3005 is up and running")
-})
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
